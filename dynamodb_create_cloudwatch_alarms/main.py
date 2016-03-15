e104fff06433a85bf71abe5364844ca77dff5554
Matthew Cox <mcox@fiftythree.com>
2016-03-15 10:29:20 -0400

    Ran into errors on older version of Python. This might be a fix?

--- dynamodb_create_cloudwatch_alarms/main.py
+++ dynamodb_create_cloudwatch_alarms/main.py
@@ -46,6 +46,7 @@ def _get_ddb_tables_list(ddb_connection):
     """
 
     ddb_tables_list_all = []
+    ddb_tables_list = {}
     ddb_tables_list = ddb_connection.list_tables()
 
     while u'LastEvaluatedTableName' in ddb_tables_list:
@@ -65,7 +66,7 @@ def _get_ddb_tables_list(ddb_connection):
             ddb_tables_list_all.extend(ddb_tables_list[u'TableNames'])
         # Boto DynamoDB v1 does not
         else:
-            ddb_tables_list_all.extend(ddb_tables_list)
+            ddb_tables_list_all.extend(ddb_tables_list.keys())
 
     return ddb_tables_list_all
 
